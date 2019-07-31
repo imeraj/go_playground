123213ff84e91cfa2c72179628c4ee0a21cbf7d2
Meraj <meraj.enigma@gmail.com>
2019-07-31 22:18:48 +0900

    - added routing support using gorilla/mux package - added custom page for 404 not found
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -3,13 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:meraj.enigma@gmail.com\">"+
+		"meraj.enigma@gmail.com</a>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>We could not find the page you "+
+		"were looking for :(</h1>"+
+		"<p>Please email us if you keep being sent to an "+
+		"invalid page.</p>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8080", http.DefaultServeMux)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	http.ListenAndServe(":8080", r)
 }
