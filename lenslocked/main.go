876b99b728dcc70c25038fc577d1fde2048b4eb1
Meraj <meraj.enigma@gmail.com>
2019-08-01 23:40:26 +0900

    - refactored code - added 404 view - added render func for view
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,32 +9,33 @@ import (
 
 var homeView *views.View
 var contactView *views.View
+var notFoundView *views.View
 
 func init() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	notFoundView = views.NewView("bootstrap", "views/errors/404.gohtml")
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
-	if err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil); err != nil {
-		panic(err)
-	}
+	must(homeView.Render(w, nil))
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
-	if err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil); err != nil {
-		panic(err)
-	}
+	must(contactView.Render(w, nil))
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>We could not find the page you "+
-		"were looking for :(</h1>"+
-		"<p>Please email us if you keep being sent to an "+
-		"invalid page.</p>")
+	must(notFoundView.Render(w, nil))
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
