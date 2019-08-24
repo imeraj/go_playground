8500bf750323ab4123ceae691e7144c69542383f
Meraj <meraj.enigma@gmail.com>
2019-08-24 10:15:55 +0900

    - fixed bug: style.css -> styles.css
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -31,7 +31,7 @@ func main() {
 
 	assetHandler := http.FileServer(http.Dir("./assets/"))
 	assetHandler = http.StripPrefix("/assets/", assetHandler)
-	r.PathPrefix("/assets").Handler(assetHandler)
+	r.PathPrefix("/assets/").Handler(assetHandler)
 
 	imageHandler := http.FileServer(http.Dir("public/images/"))
 	r.PathPrefix("/public/images/").Handler(http.StripPrefix("/public/images/", imageHandler))
