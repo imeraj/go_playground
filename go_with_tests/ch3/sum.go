c15d5a8954ec6861a413dc73ed357b2d84a914ed
Meraj <meraj.enigma@gmail.com>
2020-06-20 18:09:47 -0400

    - sum of array and slice tests added

---
+++ go_with_tests/ch3/sum.go
@@ -0,0 +1,21 @@
+package main
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	length := len(numbersToSum)
+	sums := make([]int, length)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
