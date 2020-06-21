34b3fd6bc7f02a0221fe0346bf18f1eb1643fbbc
Meraj <meraj.enigma@gmail.com>
2020-06-21 19:21:52 -0400

    - final refactored table driven tests

--- go_with_tests/ch4/shapes_test.go
+++ go_with_tests/ch4/shapes_test.go
@@ -13,22 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	areaTests := []struct {
+		name string
+		shape Shape
+		hasArea float64
+	} {
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Traingle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 06.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 }
