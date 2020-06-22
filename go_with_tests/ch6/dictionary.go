6cd1c317349c93c7875d1f438c93abe9a954e26b
Meraj <meraj.enigma@gmail.com>
2020-06-22 18:32:32 -0400

    - more map tests added

--- go_with_tests/ch6/dictionary.go
+++ go_with_tests/ch6/dictionary.go
@@ -1,5 +1,21 @@
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import "errors"
+
+var ErrNotFound = errors.New("word not found")
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
 }
--- go_with_tests/ch6/dictionary_test.go
+++ go_with_tests/ch6/dictionary_test.go
@@ -3,12 +3,45 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
+	assertString := func(t *testing.T, got, want string) {
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "word not found"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertString(t, err.Error(), want)
+
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
 	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
 
-	if got != want {
-		t.Errorf("got %q want %q given %q", got, want, "test")
+	if want != got {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
