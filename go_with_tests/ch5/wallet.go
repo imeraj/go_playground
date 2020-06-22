786687bd93aaf9148135e6c97e4a484e7af5b33a
Meraj <meraj.enigma@gmail.com>
2020-06-22 07:52:29 -0400

    - withdraw test added with error check

--- go_with_tests/ch5/wallet.go
+++ go_with_tests/ch5/wallet.go
@@ -1,13 +1,33 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
